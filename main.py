ec05ff87c88f62f3f2acb1a35b4f5536d5adaf7c
shreyanshpdixit <shreyanshdixit77@gmail.com>
2024-06-14 12:14:05 +0530

    BMI 2.0

--- main.py
+++ main.py
@@ -7,4 +7,13 @@ h = float(height)
 ans = w/(h*h)
 BMI = int(ans)
 
-print("Your BMI is: " + str(BMI))
+if BMI < 18.5:
+    print(f"Your BMI is {BMI}, you are underweight.")
+elif BMI >= 18.5 and BMI<25:
+    print(f"Your BMI is {BMI}, you have a normal weight.")
+elif BMI >= 25 and BMI<30:
+    print(f"Your BMI is {BMI}, you are slightly overweight.")
+elif BMI >= 30 and BMI<35:
+  print(f"Your BMI is {BMI}, you are obese.")
+else:
+  print(f"Your BMI is {BMI}, you are clinically obese")
